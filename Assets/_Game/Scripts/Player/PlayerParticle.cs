7ca85be1caa251ddd85e28f8d36105ef51f5d0a5
TangkwaPtc <maku4fb@gmail.com>
2023-07-01 21:45:11 +0700

    fix player particle to not attach with player and listen event from player controller instead of input system

---
+++ Assets/_Game/Scripts/Player/PlayerParticle.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace TK
+{
+    public class PlayerParticle : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _jumpParticle;
+        [SerializeField] private ParticleSystem _dashParticle;
+        [SerializeField] private ParticleSystem _attackParticle;
+        [SerializeField] private Transform _jumpTransform;
+        [SerializeField] private Transform _dashTransform;
+        [SerializeField] private Transform _attackTransform;
+
+        private PlayerController _playerController;
+
+        private void Awake()
+        {
+            _playerController = GetComponent<PlayerController>();
+        }
+
+        private void OnEnable()
+        {
+            _playerController.OnJump += PlayJumpParticle;
+            _playerController.OnDash += PlayDashParticle;
+            _playerController.OnAttack += PlayAttackParticle;
+        }
+
+        private void OnDisable()
+        {
+            _playerController.OnJump -= PlayJumpParticle;
+            _playerController.OnDash -= PlayDashParticle;
+            _playerController.OnAttack -= PlayAttackParticle;
+        }
+
+        private void PlayJumpParticle()
+        {
+            Instantiate(_jumpParticle, _jumpTransform.position, _jumpTransform.rotation);
+        }
+
+        private void PlayDashParticle()
+        {
+            Instantiate(_dashParticle, _dashTransform.position, _dashTransform.rotation);
+        }
+
+        private void PlayAttackParticle()
+        {
+            Instantiate(_attackParticle, _attackTransform.position, _attackTransform.rotation);
+        }
+    }
+}
