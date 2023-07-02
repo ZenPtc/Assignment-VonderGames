5da39bd386b1fe24588774aca7b9da46fd3d74ec
TangkwaPtc <maku4fb@gmail.com>
2023-07-02 18:09:35 +0700

    Obstacle can do repeat damage

--- Assets/_Game/Scripts/Obstacle.cs
+++ Assets/_Game/Scripts/Obstacle.cs
@@ -4,19 +4,31 @@ namespace TK
 {
     public class Obstacle : MonoBehaviour
     {
-        [SerializeField] private float dmgAmount = 5f;
-
-        private void DoDamage(Player player)
-        {
-            player.TakeDamage(dmgAmount);
-        }
+        [SerializeField] private float _dmgAmount = 10f;
+        [SerializeField] private float _repeatRate = 1f;
+        private Player _player;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if(collision.TryGetComponent(out Player player))
             {
-                DoDamage(player);
+                _player = player;
+                InvokeRepeating("DoDamage", 0, _repeatRate);
             }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if(collision.TryGetComponent(out Player player))
+            {
+                _player = null;
+                CancelInvoke("DoDamage");
+            }
+        }
+
+        private void DoDamage()
+        {
+            _player.TakeDamage(_dmgAmount);
         }
     }
 }
