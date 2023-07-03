479e8f499a3aba964538b9d7852d315fa0ed8760
TangkwaPtc <maku4fb@gmail.com>
2023-07-04 03:14:13 +0700

    set main path for night scene

--- Assets/_Game/Scripts/Player/Bullet.cs
+++ Assets/_Game/Scripts/Player/Bullet.cs
@@ -17,10 +17,10 @@ namespace TK
             transform.Translate(Vector2.right * _speed);
         }
 
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            DestroyBullet();
-        }
+        //private void OnCollisionEnter2D(Collision2D collision)
+        //{
+        //    DestroyBullet();
+        //}
 
         private void DestroyBullet()
         {
